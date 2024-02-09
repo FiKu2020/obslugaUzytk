5a2e8f3695d49f68b3de118f0ea51668160100b9
filip k <filip2006kujawa@gmail.com>
2024-02-09 11:11:19 +0100

    1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # obslugaUzytk
+
+abc
---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+import pytest
+
+from obslugaUzytk.src.app import get_user
+
+def test_get_user_function_can_be_called() -> None:
+    with pytest.raises(NotImplementedError):
+        get_user()
